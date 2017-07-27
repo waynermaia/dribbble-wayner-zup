7d2c82362cf9a84f57a665c465dae58177474780
Wayner Maia <wayner.vitorino@ipam.org.br>
2017-07-27 16:58:28 -0300

    Load dribbble

--- src/components/RequestApiDribbble.js
+++ src/components/RequestApiDribbble.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Request from 'react-http-request';
-import logo from './../logo.svg';
+import Load from './../dribbbleLogo.svg';
 
 const access_token = "32f6310e856d9e7ce2245fc5c609d6b273e6920c77489b3c3cdd018e271b3bcd";
 
@@ -16,7 +16,7 @@ export default class Dribbble extends Component {
       {
         ({error, result, loading}) => {
           if (loading) {
-            return <img src={logo} className="App-logo" alt="logo" />;
+            return <img src={Load} className="App-logo" alt="load Dribbble" />;
           } else {
             return <div>{ result.body.map((shots)=>{
               return (<div>
