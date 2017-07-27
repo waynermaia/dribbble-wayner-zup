02b20d0f36c06a13606651d6e6d5250828dc8c9e
Wayner Maia <wayner.vitorino@ipam.org.br>
2017-07-27 16:27:31 -0300

    Conhecendo a api

---
+++ src/components/RequestApiDribbble.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Request from 'react-http-request';
+import logo from './../logo.svg';
+
+const access_token = "32f6310e856d9e7ce2245fc5c609d6b273e6920c77489b3c3cdd018e271b3bcd";
+
+export default class Dribbble extends Component {
+  render() {
+    return (
+      <Request
+      url={'https://api.dribbble.com/v1/shots?&access_token=' + access_token}
+      method='get'
+      accept='application/json'
+      verbose={true}
+      >
+      {
+        ({error, result, loading}) => {
+          if (loading) {
+            return <img src={logo} className="App-logo" alt="logo" />;
+          } else {
+            return <div>{ result.body.map((shots)=>{
+              return (<div>
+                <p>
+                {shots.id}
+                </p>
+                </div>)
+            })
+          }
+          </div>;
+        }
+      }
+    }
+    </Request>
+    );
+  }
+}
